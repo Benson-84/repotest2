8070195bb2dad7c9822536e2ac53556d316c62df
Benson Xu <benson.xu@wework.com>
2020-04-14 09:46:08 +0800

    init main UI layout

--- renderer/src/index.tsx
+++ renderer/src/index.tsx
@@ -1,14 +1,38 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import "./style.css";
+import './style.css'
+import SlideBar from './components/sidebar/sidebar'
 
+interface State {
+  activeUser: any
+}
 
-class App extends React.Component {
+class App extends React.Component<any, State> {
+  constructor(props: any) {
+    super(props)
+
+    // todo load information of active user here
+    // this.state = {
+    //   activeUser: 'somebody'
+    // }
+  }
+
   render() {
-    return (
-      <webview className="miniAppContainer" src="../miniapps/miniapp-login/index.html"></webview>
-    )
+    if (this.state.activeUser) {
+      return (
+        <div className="main-container">
+          < div className='sidebar-container'>
+            <SlideBar />
+          </div>
+          <div className='content-container'></div>
+        </div>
+      );
+    } else {
+      return (
+        <webview className="miniAppContainer" src="../miniapps/miniapp-login/index.html"></webview>
+      );
+    }
   }
 }
- 
+
 ReactDOM.render(<App />, document.getElementById('app'));
